74f6e4f9d3a3756196c7d39f7101b17be9f649b0
Vinicius Oliveira de Freitas <vini@MacBook-Air-de-Vinicius.local>
2023-02-28 17:48:27 -0300

    Componente de comentários

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/8822046?v=4" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Vinicius Oliveira</strong>
+              <time dateTime="2022-05-11 08:13:00" title="11 de maio as 08:13h">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp /> Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
